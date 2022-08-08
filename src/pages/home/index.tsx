c6b3d1ed2d5d51d798aa1df8f1d7232c321acaaa
Pedro26fer <p2698fernandes@gmail.com>
2022-08-08 14:30:08 -0300

    portfólio_primeira_versão

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -32,7 +32,7 @@ export const Home = (): JSX.Element => {
         <Container>
           <HeaderContent>
             <Text as="h1" type="heading1" color="grey5">
-              Criando experiências por meio da tecnologia{" "}
+              Buscando soluções por meio da tecnologia{" "}
             </Text>
             <Text type="body1" color="grey6">
               Sou estudante de programação na Kenzie Academy Brasil, participei
